9872970d1425191e8020ff8a4f62f3712602e8fe
Gihwan Kim <ghkim3221@gmail.com>
2019-02-19 22:47:48 +0900

    Fragment들 추가 (Playing, Popular, Profile)

--- app/src/main/java/org/openpnu/gopnu/ui/PlayingFragment.java
+++ app/src/main/java/org/openpnu/gopnu/ui/PlayingFragment.java
@@ -1,7 +1,30 @@
 package org.openpnu.gopnu.ui;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import org.openpnu.gopnu.R;
 
 public class PlayingFragment extends Fragment {
 
+  private TextView mTitleView;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_playing, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    mTitleView = view.findViewById(R.id.playing_title);
+  }
 }
