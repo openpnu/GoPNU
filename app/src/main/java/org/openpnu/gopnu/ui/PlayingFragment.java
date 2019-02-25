e39b4615d8a4f309f0833833eb5d2487677b4f21
Seo-GwangHyeon <dya2122@gmail.com>
2019-02-25 12:29:42 +0900

    Google Fit API 임시 추가
    걸음 수와 걸은 거리 측정하는 Google Fit API를 Playing Fragment에 버튼을 만들어, 그 버튼으로 연결되는 Activity에 임시로 추가하였다.

--- app/src/main/java/org/openpnu/gopnu/ui/PlayingFragment.java
+++ app/src/main/java/org/openpnu/gopnu/ui/PlayingFragment.java
@@ -1,19 +1,21 @@
 package org.openpnu.gopnu.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import org.openpnu.gopnu.R;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class PlayingFragment extends Fragment {
+public class PlayingFragment extends Fragment implements View.OnClickListener {
 
   private TextView mTitleView;
+  private Button mStepButton;
 
   @Nullable
   @Override
@@ -25,7 +27,18 @@ public class PlayingFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-
     mTitleView = view.findViewById(R.id.playing_title);
+    mStepButton = view.findViewById(R.id.step_button);
+    mStepButton.setOnClickListener(this);
   }
+
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        if(id==R.id.step_button)
+        {
+            Intent intent=new Intent(getContext(), StepActivity.class);
+            startActivity(intent);
+        }
+    }
 }
